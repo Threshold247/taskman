36dc227aee8ba27d08a638733ffd58f79cb695d1
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2021-09-10 17:27:42 +0200

    Converted end points to async functions

--- Routes/tasks.js
+++ Routes/tasks.js
@@ -11,36 +11,42 @@ const pool = new Pool({});
 
 router
 	.route("/")
-	.get(function (req, res) {
+	.get(async function(req, res) {
 		console.log(req.method,req.originalUrl)
-		pool
-		.query("SELECT * FROM tasks")
-		.then((result) => res.json(result.rows))
-		.catch((error) => console.log(error));
-
+		try {
+			const result = await pool.query("SELECT * FROM tasks");
+			const data = await res.json(result.rows);
+		
+		}catch(error) {
+			 console.log(error);
+		}
 })
-	.post(function (req,res) {
+	.post(async function (req,res) {
 		console.log(req.method,req.originalUrl)
-		const {description, day, reminder} =req.body;
-		const addTaskQuery = "INSERT INTO tasks (description,day,reminder) VALUES ($1,$2,$3)";
+		try {
+			const {description, day, reminder} =req.body;
+			const addTaskQuery = "INSERT INTO tasks (description,day,reminder) VALUES ($1,$2,$3)";
 
-		pool
-			.query(addTaskQuery, [description,day,reminder])
-			.then((result) => res.json(result.rows))
-			.catch((error) => console.log(error));
-
+			const result = await pool.query(addTaskQuery, [description,day,reminder]);
+			const data = await res.json(result.rows);
+		} catch(error) {
+			console.log(error);
+		}
 })
 
 router
     .route("/:id")
-    .get(function (req, res) {
+    .get(async function (req, res) {
   	 console.log(req.method,req.originalUrl)
-  	    const {id} = req.params
-  	    const query = "SELECT * FROM tasks WHERE id =$1";
-        pool
-			.query(query, [id])
-			.then((result) => res.json(result.rows))
-			.catch((error) => console.log(error));
+  	    try {
+			const {id} = req.params
+			const query = "SELECT * FROM tasks WHERE id =$1";
+			const result = await pool.query(query, [id]);
+			const data = await res.json(result.rows)
+			
+		}catch(error){
+			console.log(error);
+		}	
 })
 
 	.delete(function (req,res) {
